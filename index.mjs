ebf58f4a23c43b5cc766069b48328cb3959f973a
lupinelab <jed@lupinelab.co.uk>
2024-10-16 11:28:10 +0100

    feat: add log levels and debug logging

--- index.mjs
+++ index.mjs
@@ -3,28 +3,37 @@ import { exit } from "process";
 import { LUCI } from "./luci.mjs";
 import * as eufyrobovac from "eufy-robovac";
 
-const POLL_INTERVAL = process.env["POLL_INTERVAL"] ?? 1000 * 60;
-const MONITORED_MACADDRESSES = process.env["MONITORED_MACADDRESSES"]
-  ? process.env["MONITORED_MACADDRESSES"].toUpperCase().split(",")
+const LOG_LEVEL =
+  process.env.LOG_LEVEL === "debug"
+    ? pino.levels.values.debug
+    : pino.levels.values.info;
+const ROBOVAC_DEBUG = !!process.env.ROBOVAC_DEBUG;
+
+const POLL_INTERVAL = process.env.POLL_INTERVAL ?? 1000 * 60;
+const MONITORED_MACADDRESSES = process.env.MONITORED_MACADDRESSES
+  ? process.env.MONITORED_MACADDRESSES.toUpperCase().split(",")
   : [];
 
-const OPENWRT_HOST = process.env["OPENWRT_HOST"];
-const OPENWRT_USERNAME = process.env["OPENWRT_USERNAME"] ?? "root";
-const OPENWRT_PASSWORD = process.env["OPENWRT_PASSWORD"];
+const OPENWRT_HOST = process.env.OPENWRT_HOST;
+const OPENWRT_USERNAME = process.env.OPENWRT_USERNAME ?? "root";
+const OPENWRT_PASSWORD = process.env.OPENWRT_PASSWORD;
 
-const ROBOVAC_DEVICE_ID = process.env["ROBOVAC_DEVICE_ID"];
-const ROBOVAC_LOCAL_KEY = process.env["ROBOVAC_LOCAL_KEY"];
-const ROBOVAC_IP = process.env["ROBOVAC_IP"];
+const ROBOVAC_DEVICE_ID = process.env.ROBOVAC_DEVICE_ID;
+const ROBOVAC_LOCAL_KEY = process.env.ROBOVAC_LOCAL_KEY;
+const ROBOVAC_IP = process.env.ROBOVAC_IP;
 
 const logger = pino.pino({
   base: null,
   timestamp: pino.stdTimeFunctions.isoTime,
+  level: LOG_LEVEL,
 });
 
 async function main() {
   if (MONITORED_MACADDRESSES.length === 0) {
     throw new Error("No monitored mac addresses are set");
   }
+
+  logger.debug({ monitoredMacaddressed: MONITORED_MACADDRESSES });
 
   const luci = new LUCI(
     `https://${OPENWRT_HOST}`,
@@ -43,7 +52,7 @@ async function main() {
     ip: ROBOVAC_IP,
   };
 
-  let robovac = new eufyrobovac.RoboVac(robovacConfig, false);
+  let robovac = new eufyrobovac.RoboVac(robovacConfig, ROBOVAC_DEBUG);
   let svenomaticTriggeredRobovac = false;
   let poll = true;
 
@@ -59,6 +68,8 @@ async function main() {
 
   while (poll) {
     const wlanInterfaces = await luci.getWlanDevices();
+    logger.debug({ wlanInterfaces: wlanInterfaces });
+
     const connectedMacAddresses = (
       await Promise.all(
         wlanInterfaces.map(async (iface) => {
@@ -67,9 +78,10 @@ async function main() {
         })
       )
     ).flat();
+    logger.debug({ connectedMacAddresses: connectedMacAddresses });
 
     await robovac.getStatuses();
-    logger.info({
+    logger.debug({
       robovacStatus: robovac.statuses.dps[robovac.WORK_STATUS].toLowerCase(),
     });
 
